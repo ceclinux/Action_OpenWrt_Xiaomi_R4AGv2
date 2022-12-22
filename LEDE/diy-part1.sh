2425cfecd7f29f41bb00b44b7b4e609c9056be06
ceclinux <ceclinux@users.noreply.github.com>
2022-12-22 15:42:05 +0800

    Update diy-part1.sh

--- LEDE/diy-part1.sh
+++ LEDE/diy-part1.sh
@@ -12,7 +12,7 @@
 # 添加插件源码
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 ### 修改主题文件
 rm -rf package/lean/luci-theme-argon
